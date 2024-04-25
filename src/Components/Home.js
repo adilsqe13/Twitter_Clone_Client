8f4f51500ce10960add34f583a43328f8736175f
adilsqe13 <adilsqe13@gmail.com>
2024-04-25 11:09:46 +0530

    repost fixing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -235,9 +235,9 @@ export default function Home() {
                                     {/* Repost */}
                                     <div className='col-4 dfjcac p-0'>
                                         <div className=" bold fs-5  mwfkro ">
-                                            <a href='/home' onClick={() => { handleRepost(item._id) }} className=' bg-transparent  text-secondary text-decoration-none' title='Repost'> <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" viewBox="0 0 16 16">
+                                            <button  onClick={() => { handleRepost(item._id); }} className=' border-0 bg-transparent  text-secondary text-decoration-none' title='Repost'> <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" viewBox="0 0 16 16">
                                                 <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192Zm3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z" />
-                                            </svg> <span className='fs-6'>{item.RepostBy.length}</span></a>
+                                            </svg> <span className='fs-6'>{item.RepostBy.length}</span></button>
                                         </div>
                                     </div>
 
